5fe8e04194efae38e569b02bea880ded593b536a
Mohammed-AhmedAF <mohamedstarwars@gmail.com>
2025-04-03 15:12:10 +0200

    Moving files to firstTest

--- firstTest/main.cpp
+++ firstTest/main.cpp
@@ -1,6 +1,11 @@
 #include <gtest/gtest.h>
 
 
+TEST(FirstTest,TestOne)
+{
+    ASSERT_EQ(1+1,2);
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
